9f5c02658614aa7a5edd0b85ad18ce89b82798b2
Benjamin <work@benjaminrussell.me>
2021-07-02 15:14:00 -0500

    build: inputs and file stuff

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 use structopt::StructOpt;
-use std::io::BufReader;
-use std::fs::File;
+use std::io::{stdin};
 #[derive(StructOpt)]
 struct Cli {
     pattern: String,
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf
 }
-fn main() {
+fn input() {
+  let mut input = String::new();
+  stdin().read_line(&mut input).expect("error: unable to read user input");
+  println!("You typed: {}",input);
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+input();
 let args = Cli::from_args();
 
-let f = File::open(&args.path);
-let content = BufReader::new(f)
-    .expect("could not read file");
-
+let result = std::fs::read_to_string(&args.path);
+let content = match result {
+    Ok(content) => {content},
+    Err(error) => {panic!("error time!! {}", error)}
+};
 for line in content.lines() {
     if line.contains(&args.pattern) {
-        println!("{}", line)
+        println!("{}", line);
     }
 }
+Ok(())
 }
