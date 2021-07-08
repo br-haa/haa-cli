1345e6246a18137685b8fb2c684ba2efffd696a6
Benjamin <work@benjaminrussell.me>
2021-07-08 15:16:43 -0500

    test: writing over files

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,39 @@
 use dialoguer::{theme::ColorfulTheme, MultiSelect};
-use std::process::Command;
 use std::fs::File;
+use std::io::prelude::*;
+use std::process::Command;
 
-fn main () -> std::io::Result<()>{
-    let multi = &[
-        "car",
-        "bike",
-        "plane",
-        "roller blades"
-    ];
+fn main() -> std::io::Result<()> {
+    let multi = &["car", "bike", "plane", "roller blades"];
     let selections = MultiSelect::with_theme(&ColorfulTheme::default())
         .with_prompt("how move?")
         .items(&multi[..])
         .interact()
         .unwrap();
 
-        if selections.is_empty() {
-            println!("hello?")
-        } else  {
-            println!("You selected these things:");
-            for selection in selections {
-                let m = multi[selection];
-                println!(" {}", m);
+    if selections.is_empty() {
+        println!("hello?")
+    } else {
+        println!("You selected these things:");
+        for selection in selections {
+            let m = multi[selection];
+            println!(" {}", m);
 
-                if m == "bike" {
-                Command::new("ls")
-                    .spawn()
-                    .expect("idk");
-                } else if m == "plane" {
-                    File::create("text.txt");
-                }
+            if m == "bike" {
+                Command::new("ls").spawn().expect("idk");
+            } else if m == "plane" {
+                let mut f = File::create("text.txt")?;
+                f.write_all(b"plane")?;
+                f.sync_all()?;
+            } else if m == "roller blades" {
+                Command::new("git").arg("status").spawn().expect("xd");
+            } else if m == "car" {
+                let mut f = File::create("text.txt")?;
+                f.write_all(b"car")?;
+                f.sync_all()?;
             }
         }
-            
-Ok(())
+    }
+
+    Ok(())
 }
